34dbc4882d6e25d5f0942c19a4318e2e39a76774
ASUS\USER <raer1332@gmail.com>
2018-04-26 18:34:48 +0800

    no message

--- 419edited/MainWindow.xaml.cs
+++ 419edited/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace _419edited
         {
             InitializeComponent();
         }
+        //set blank of heightbox
+        private void heightbutton_Click(object sender, RoutedEventArgs e)
+        {
+            heightbox.Background = Brushes.Yellow;
+            heightbox.Text = "";
+        }
+        //set blank of weightbox
+        private void weightbutton_Click_1(object sender, RoutedEventArgs e)
+        {
+            weightbox.Background = Brushes.Yellow;
+            weightbox.Text = "";
+        }
+
+       
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (IsInitialized)
+            {
+                heightbox.Text = slider_h.Value.ToString("#00.00").ToString(); // change value by hand    
+                weightbox.Text = slider_w.Value.ToString("#00.00").ToString();
+
+            double w = double.Parse(weightbox.Text);
+            double h = double.Parse(heightbox.Text);
+            double BMI = 10000*w / (h * h);
+            answer.Text= BMI.ToString("#00.00").ToString();
+            }
+        }
+
+     
     }
 }
